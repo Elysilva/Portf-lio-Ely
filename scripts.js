2c42e94d756ca8875ac2d2886803e24f2425e80a
Ely Silva <105675847+Elysilva@users.noreply.github.com>
2022-09-05 19:53:04 -0300

    Meu Portfólio

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function sobreMim(){
+    window.location = "index2.html";
+}
+
+function redirecionaProjeto() {
+    window.location = "index3.html";
+
+}
+  
+function carLinkedin(){
+    document.getElementsByClassName("button")
+    .innerHTML = "https://www.linkedin.com/in/ely-almeida-da-silva-9739971a0"
+
+}
+
+function carGithub(){
+    document.getElementsByClassName("centro")
+    .innerHTML = "https://github.com/Elysilva"
+}
